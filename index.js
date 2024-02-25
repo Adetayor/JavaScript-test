31b57eb2cea53ab4f9ed4a3683cb74676ec97eff
Samuel <adetayo456@gmail.com>
2024-02-25 09:07:55 +0100

    Mark and John BMI Compared

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ let population = "3,490,621";
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let massMark = "78kg";
+let heightMark = "1.69m";
+let massJohn = "92kg";
+let heightJohn = "1.95m";
+
+let BMIMark = 78 / (1.69 * 1.69);
+let BMIJohn = 92 / (1.95 * 1.95);
+
+console.log(BMIMark, BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+console.log('Mark has the highest BMI of' + ' ' + BMIMark);
