fb12d19aeaa7a1c4c30bbc09e260281eeede9260
Samuel <adetayo456@gmail.com>
2024-04-04 10:05:36 +0100

    Exercise 6

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
-*/
+
 
 //Challenge 5
 const mark = {
@@ -190,3 +190,32 @@ if(mark.bmi > john.bmi) {
 } else if (john.bmi > mark.bmi) {
   console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
 }
+*/
+
+const calcTip = function(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 185, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    //sum = sum + arr(i);
+    sum += arr[i];
+  }
+  //console.log(sum);
+  return sum / arr.length;
+}
+//console.log(calcAverage([2, 3, 6]));
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
